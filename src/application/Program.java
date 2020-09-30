0259b0e431a543e4dd3e7506904ff7fcba0d29c7
Walter dos Santos <walter4asantos@gmail.com>
2020-09-30 14:39:31 -0300

    Implementacao de objeto Comparator com a expressao Lambda direto nos argumentos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -16,10 +15,8 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); 
-		//Como a implementacao foi feita em uma única linha, posso dispensar as chaves '{}' e palavra 'return'
-		
-		list.sort(comp); 
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase())); 
+		// Implementacao de objeto Comparator com a expressao Lambda direto nos argumentos
 		
 		for (Product p : list) {
 			System.out.println(p);
